a12e2c70c87906a28af7eb29b2248170f1122766
이혜원 <mvat1225@gmail.com>
2022-09-19 20:04:06 +0900

    20220919
    서버 마저 구현하고 람다.,..공ㅂ.... 알고리즘....할까 과연??

---
+++ Assets/Script/Prototype/LobbyManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    TextMeshProUGUI nameText;
+  
+    public void Connect()
+    {
+        PhotonNetwork.LocalPlayer.NickName = nameText.text;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 20 },null);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("접속 실패");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("레벨 로딩");
+
+        PhotonNetwork.LoadLevel("Map");
+    }
+}
